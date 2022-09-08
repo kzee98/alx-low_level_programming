6f5fcc9bc5d404382894ff40f224752ca7b5ff55
kzee98 <kzmzorodzi7@gmail.com>
2022-09-08 12:20:45 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	char str1{} = "\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+	return (0);
 }
