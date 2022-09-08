84fd55f6b7c6ac4fc86d00cccce1dab3a1fcf3af
kzee98 <kzmzorodzi7@gmail.com>
2022-09-08 05:29:51 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 after the function
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
