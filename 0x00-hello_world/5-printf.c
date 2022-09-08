ad8ee6fd2f4a87986011cfaa2cb6f1f24f57cbd4
kzee98 <kzmzorodzi7@gmail.com>
2022-09-08 14:54:01 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
-#include<stdio.h>                                                                                                                    
-#include<string.h>                                                  
+#include <stdio.h>                                                                                                                    
+#include <string.h>                                                  
+
 /**
  * main - Entry point
  *
- * Return:Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
