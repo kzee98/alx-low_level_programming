1d4667d95f18e6c8d2793b4353c7134e5edb2d89
kzee98 <kzmzorodzi7@gmail.com>
2022-09-09 13:15:51 -0700

    AXL

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numb;
+	char alphab;
+
+	for (numb = 48 ; numb <= 57 ; numb++)
+		putchar(numb);
