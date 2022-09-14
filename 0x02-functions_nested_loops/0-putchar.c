e072187fb11474c5e1b39970f5695a2d190cab7f
kzee98 <kzmzorodzi7@gmail.com>
2022-09-14 08:03:05 -0700

    ALX

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,30 +1,21 @@
-#include <unistd.h>
-#include "main.h"
+include "main.h"
 
 /**
- * main - Entry point
+ * main - print _putchar
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
 int main(void)
 {
-	char ch[8] = "_putchar";
-		int i = 0;
-		char c;
-
-		while(i<= 8){
-			c = ch[i];
-			_putchar(c);
-			i++;
-
-}
-{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
-	return (0);
-}
-
-int _putchar(char c)
-{
-return (write(1, &c, 1));
+	return(0);
 }
