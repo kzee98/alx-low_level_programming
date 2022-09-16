8484954797e5acafc69fb14a5d5c01c9383cbcfa
kzee98 <kzmzorodzi7@gmail.com>
2022-09-16 13:04:21 -0700

    Alx

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - 0-9 except for
+ */
+void print_most_numbers(void)
+{	
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar((i %
+
+	}
+
+	_putchar('\n');
+}
