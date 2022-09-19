08fd61c8057fcf790b16ad90cbcbe9df4ea9a65d
kzee98 <kzmzorodzi7@gmail.com>
2022-09-19 09:28:30 -0700

    Alx

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,9 @@
  */
 void print_rev(char *s)
 {
-	int i, len;
-	
-	len = strlen(s);
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);	
-
-	_putchar('\n');
+int i, len;
+len = strlen(s);
+for (i = len - 1; i >= 0; i--)
+_putchar(s[i]);	
+_putchar('\n');
 }
