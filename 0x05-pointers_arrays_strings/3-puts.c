281ecafde5d3d045d08f462dcb7c6baae58ff305
kzee98 <kzmzorodzi7@gmail.com>
2022-09-19 03:23:32 -0700

    Alx

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
 /**
- * _puts - writr a function that prints a string, followed by a new line, to 
+ *_puts-writr a function that prints a string, followed by a new line, to 
  * stdout.
- *
  * @str: This is my input string
  *
  */
-
 void _puts(char *str)
 {
 	int index;
