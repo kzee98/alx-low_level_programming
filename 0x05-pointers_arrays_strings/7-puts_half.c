047c29a37bbb1d66856dd945b8b79a18426f1c91
kzee98 <kzmzorodzi7@gmail.com>
2022-09-19 15:04:23 -0700

    Alx

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * puts2 - prints every other character of a  string to stdout
  * @str: string to print
@@ -8,12 +7,11 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
-
-	while (*(str + i))
-	{
-		_putchar(*(str + i));
-		i = i + 2;
-	}
-	_putchar('\n');
+int i = 0;
+while (*(str + i))
+{
+_putchar(*(str + i));
+i = i + 2;
+}
+_putchar('\n');
 }
