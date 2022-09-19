88c95e394d40e73d3d033e51ffe53bff3b5be2c7
kzee98 <kzmzorodzi7@gmail.com>
2022-09-19 14:50:16 -0700

    Alx

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - input num of ele
+ * @a: array
+ * @n: elements to print
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{ 
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+	}
