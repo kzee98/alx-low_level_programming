8b5ad6b1f57c367baecaf6f0c209600a592a6566
kzee98 <kzmzorodzi7@gmail.com>
2022-09-19 15:21:10 -0700

    Alx

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,13 +5,12 @@
  * @str: This is my input string
  *
  */
-void _puts(char *str)
+void _puts(char*str)
 {
-	int index;
-
-	for (index = 0; str[index] != '\0'; index++)
-	{
-		_putchar(str[index]);
-	}
-	_putchar('\n');
+int index;
+for(index =0; str[index]!= '\0'; index++)
+{
+_putchar(str[index]);
+}
+_putchar('\n');
 }
