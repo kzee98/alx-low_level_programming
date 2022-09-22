db288418710ed064fb37b96c00aec38b475e988c
kzee98 <kzmzorodzi7@gmail.com>
2022-09-22 06:42:34 -0700

    ALX

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,8 @@
  *
  * Return:returns pointer to @dest
  */
-char*_strcat(char*dest,char*src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len =0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
-return (dest);
+	strcat(dest, src);
+	return (dest);
 }
