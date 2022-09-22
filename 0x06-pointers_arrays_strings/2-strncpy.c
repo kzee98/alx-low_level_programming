aec50e888dfdcfb7bd511edf548175435a039660
kzee98 <112075202+kzee98@users.noreply.github.com>
2022-09-22 15:58:52 +0200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,43 +1,25 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string.
- * @str: The string to be encoded.
+ * _strncpy - Copy input
+ * @dest: string1.
+ * @src: string 2.
+ * @n: inte
  *
- * Return: A pointer to the encoded string.
+ * Return: dest.
  */
-char *rot13(char *str)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-			    'G', 'H', 'I', 'J', 'K', 'L',
-			    'M', 'N', 'O', 'P', 'Q', 'R',
-			    'S', 'T', 'U', 'V', 'W', 'X',
-			    'Y', 'Z', 'a', 'b', 'c', 'd',
-			    'e', 'f', 'g', 'h', 'i', 'j',
-			    'k', 'l', 'm', 'n', 'o', 'p',
-			    'q', 'r', 's', 't', 'u', 'v',
-			    'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-			     'T', 'U', 'V', 'W', 'X', 'Y',
-			      'Z', 'A', 'B', 'C', 'D', 'E',
-			      'F', 'G', 'H', 'I', 'J', 'K',
-			      'L', 'M', 'n', 'o', 'p', 'q',
-			      'r', 's', 't', 'u', 'v', 'w',
-			      'x', 'y', 'z', 'a', 'b', 'c',
-			      'd', 'e', 'f', 'g', 'h', 'i',
-			      'j', 'k', 'l', 'm'};
-	
-	while (str[indx1])
-	{
-		for (indx2 = 0; indx2 < 52; indx2++)
-		{
-			if (str[indx1] == alphabet[indx2])
-			{
-				str[indx1] = rot13key[indx2];
-				break;
-			}
-		}
-		
-		indx1++;
-	}
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
