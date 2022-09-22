5551b0f8b0e6bb09ea88053a06c42fcb830b75dc
kzee98 <kzmzorodzi7@gmail.com>
2022-09-22 01:35:16 -0700

    Als

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - rev arr
+ * @a: arr
+ * @n: elements
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, i;
+
+	for (i = n - 1; i > n / 2; i--)
+	{
+		tmp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = tmp;
+	}
+}
