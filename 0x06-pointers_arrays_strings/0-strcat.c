8d2fa3f8f20a0ae7a345891fa0abf4833d00fc78
kzee98 <kzmzorodzi7@gmail.com>
2022-09-22 00:27:31 -0700

    Alx

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concanete string
+ * @dest: string 1
+ * @src: string 2
+ *
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
