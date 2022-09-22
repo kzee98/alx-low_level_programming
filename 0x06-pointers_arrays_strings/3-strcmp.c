2dd996fa88a85c9698fe54ac0dbe91f3d7c5fd02
kzee98 <kzmzorodzi7@gmail.com>
2022-09-22 01:29:19 -0700

    Alx

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare
+ * @s1: string 1
+ * @s2: string 2
+ * Return: val
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int tmp;
+
+	tmp = strcmp(s1, s2);
+	return (tmp);
+}
