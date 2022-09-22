37a9be6d72a6df6444388981f36b15062d3cab4a
kzee98 <kzmzorodzi7@gmail.com>
2022-09-22 01:56:13 -0700

    Alx

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - low to up
+ * @str: param 1
+ * Return: character
+ */
+char *string_toupper(char *str)
+{ 
+	int i;
+
+	i = 0;
+
+	while (str[i++])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+
+	return (str);
+}
