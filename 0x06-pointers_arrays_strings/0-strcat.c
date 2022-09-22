e64ca088b537420621d1f2fa3c0fed039625f0a7
kzee98 <kzmzorodzi7@gmail.com>
2022-09-22 00:43:07 -0700

    Alx

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,10 +6,17 @@
  * @dest: string 1
  * @src: string 2
  *
- * Return: string
+ * Return:returns pointer to @dest
  */
-char *_strcat(char *dest, char *src)
+char*_strcat(char*dest,char*src)
 {
-strcat(dest, src);
+	int index = 0, dest_len =0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 return (dest);
 }
