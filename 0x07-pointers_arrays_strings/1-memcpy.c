38616f444ac90be9790204d4c58cab5c793ee980
kzee98 <kzmzorodzi7@gmail.com>
2022-09-27 11:05:21 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memset - fill memory
+ *  @s: param1
+ *  @b: param2
+ *  @n: param3
+ *
+ *   Return: memset
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+	return (s);
+}
