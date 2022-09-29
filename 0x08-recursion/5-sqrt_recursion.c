59c3a735d0ca1a8559348c8e5cbb3667be3a62db
kzee98 <kzmzorodzi7@gmail.com>
2022-09-28 23:51:23 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print length
+ * @s: string
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	return (len);
+}
